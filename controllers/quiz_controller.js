8615f0d874ae9076d0d29954c4865bf713b5aac3
Juan Cerezo <jcerezotic@gmail.com>
2015-07-13 23:02:50 +0200

    Mistake solved
    Copy&Paste is very dangerous! D:

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,8 +6,8 @@ exports.question = function(req, res) {
 // GET /quizes/answer
 exports.answer = function(req, res) {
   if (req.query.respuesta === 'Roma'){
-    res.render('quizes/answer', {pregunta: 'Correcto'});
+    res.render('quizes/answer', {respuesta: 'Correcto'});
   } else {
-    res.render('quizes/answer', {pregunta: 'Incorrecto'});
+    res.render('quizes/answer', {respuesta: 'Incorrecto'});
   };
 };
