58cb153d3b77e6318fd88a2ece2db5eaf8eac153
Juan Cerezo <jcerezotic@gmail.com>
2015-08-14 22:47:39 +0200

    Parche de simplificación para busquedas
    Al parecer, un error está siendo ocasionado por la función de búsqueda
    de quizes. Parche de simplificación: si no hay coincidencia el resultado
    se muestra como vacío. No hay mensaje de no encontrado.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,13 +25,17 @@ exports.index=function(req,res){
     var _mask = {where: ['pregunta like ?','%' + req.query.search.replace(" ","%") + '%'], order:'pregunta ASC'};
     models.Quiz.findAll(_mask).then(
             function(quizes){
-                if (quizes.length === 0) {
-                    console.log('quiz_controller: ' + quizes.length + ' quizes encontrados utilizando .index');
-                    res.render('quizes/notfound.ejs',{quizes: 'No hay resultados para:' + '"' + req.query.search + '"' });
-                } else {
-                    console.log('quiz_controller: ' + quizes.length + ' quizes encontrados utilizando .index');
-                    res.render('quizes/index.ejs',{quizes:quizes});
-                }
+              /*
+              if (quizes.length === 0) {
+                  console.log('quiz_controller: ' + quizes.length + ' quizes encontrados utilizando .index');
+                  res.render('quizes/notfound.ejs',{quizes: 'No hay resultados para:' + '"' + req.query.search + '"' });
+              } else {
+                  console.log('quiz_controller: ' + quizes.length + ' quizes encontrados utilizando .index');
+                  res.render('quizes/index.ejs',{quizes:quizes});
+              }
+              */
+              console.log('quiz_controller: ' + quizes.length + ' quizes encontrados utilizando .index');
+              res.render('quizes/index.ejs',{quizes:quizes});
             }).catch(function(error){next(error);});
   };
 };
