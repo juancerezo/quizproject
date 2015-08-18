716a62947d4070d07aaf2246cdae793b347c42b2
Juan Cerezo <jcerezotic@gmail.com>
2015-08-19 01:43:52 +0200

    Nueva función para añadir preguntas
    Ahora se pueden añadir preguntas desde la sección de preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -50,6 +50,25 @@ exports.answer = function (req, res) {
   res.render('quizes/answer',{ quiz: req.quiz, respuesta: resultado});
 };
 
+// GET /quizes/new
+exports.new = function (req, res) {
+  var quiz = models.Quiz.build(//crea objeto quiz
+    {pregunta: "", respuesta: ""}
+  );
+  res.render('quizes/new', {quiz: quiz});
+}
+
+//POST /quizes/create
+
+exports.create = function (req, res) {
+  var quiz = models.Quiz.build(req.body.quiz);
+
+  //guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  }) //Redirección HTTP (URL relativa) lista de preguntas
+};
+
 // GET /author
 exports.author = function(req, res) {
   res.render('author', {autor: 'Juan Cerezo',
